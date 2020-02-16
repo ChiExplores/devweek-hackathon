359340b68ce6b0f6dc8947594be4e7766e46e1c4
Chi Huynh <contact@chihuynh.me>
2020-02-15 21:14:56 -0800

    search bar in form

--- client/src/Mapped.js
+++ client/src/Mapped.js
@@ -3,7 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 import SideBar from "./SideBar";
 import DisplayMap from "./DisplayMap";
 
-const Mapped = () => {
+const Mapped = (props) => {
   return (
     <div>
       <SideBar />
